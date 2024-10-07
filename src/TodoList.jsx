658e677d42e64d7a53fbc2320d42ab086a1ded50
Hilla Nokela <mikko.vitikka@myy.haaga-helia.fi>
2024-10-07 20:57:58 +0300

    Exercise complete

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,50 +1,51 @@
 import { useState } from "react";
 import TodoTable from "./TodoTable";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Container from '@mui/material/Container';
 
 function TodoList() {
-    const [desc, setDesc] = useState("");
     const [todos, setTodos] = useState([]);
-    const [date, setDate] = useState("");
+    const [newTodo, setNewTodo]  = useState({
+        desc: '',
+        priority: '',
+        date: ''
+    })
     
 
-    const handleDesc = (event) => {
-        setDesc(event.target.value);
+    const newInput = (event) => {
+        setNewTodo({...newTodo, [event.target.name]: event.target.value});
         
         
         
     };
-    const handleDate =(event) =>{
-        setDate(event.target.value);
-        
-        
-        
-    }
+    
     const addTodo = () => {
-        if (!desc || !date){
+        if (!newTodo.desc || !newTodo.date){
             alert('Write a description and date for a todo item')
             return;
         }
+        
+        setTodos([...todos, newTodo]);
+        setNewTodo({desc:'', priority:'', date:''})
+        
+        
 
-        const newItem = {
-            date1: date,
-            desc1: desc
-        }
-        
-        
-        setTodos((prevTodos) => [...todos, newItem]);
-        
-        setDesc("")
-        setDate("")
-        
-        
-        
-        
-        
         
     };
+    
 
-    const removeTodo = (index) => {
-        setTodos((prevTodos) => prevTodos.filter((todo, i) => i !== index));
+    const [selectedTodoIndex, setSelectedTodoIndex] = useState(null);
+
+    
+    const removeTodo = () => {
+        if (selectedTodoIndex !== null) {  
+            setTodos((prevTodos) => prevTodos.filter((todo, i) => i !== parseInt(selectedTodoIndex)));
+            setSelectedTodoIndex(null);
+        } else {
+            alert('Select a row first!');
+        }
     };
 
     return (
@@ -55,11 +56,13 @@ function TodoList() {
         </div>
         <fieldset id="ToDoInput">
             <legend>Add Todo</legend>
-            <input placeholder="Description" onChange={handleDesc} value={desc} />
-            <input placeholder="Date" onChange={handleDate} value={date}/>
+            <input type="text" placeholder="Description" name="desc" onChange={newInput} value={newTodo.desc} />
+            <input type="text" placeholder="Priority" name="priority" onChange={newInput} value={newTodo.priority}/>
+            <input type="date" placeholder="Date" name="date" onChange={newInput} value={newTodo.date}/>
             <button onClick={addTodo}>Add</button>
+            <button onClick={removeTodo}>Delete</button>
         </fieldset>
-            <TodoTable todos={todos} removeTodo={removeTodo}/>
+            <TodoTable todos={todos} setSelectedTodoIndex={setSelectedTodoIndex}/>
             
         </>
     );
--- src/TodoTable.jsx
+++ src/TodoTable.jsx
@@ -1,31 +1,56 @@
 import React from "react";
 
-import TodoList from "./TodoList";
 
-function TodoTable({todos, removeTodo}) {
-    
+import TodoList from "./TodoList";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
+
+import { useState, useRef } from "react";
+
+function TodoTable({ todos, setSelectedTodoIndex }) {
+    const [columnDefs] = useState([
+        { field: 'desc', sortable: false, filter: true, floatingFilter: true },
+        {
+            field: 'priority', filter: true, floatingFilter: true,
+            cellStyle: params => params.value === "High" ? { color: 'red' } : { color: 'black' }
+        },
+        { field: 'date', filter: true, floatingFilter: true }
+    ]);
+
+    const gridRef = useRef();
+
+    const onRowSelected = () => {
+        const selectedNodes = gridRef.current.getSelectedNodes();
+        if (selectedNodes.length > 0) {
+            const selectedIndex = selectedNodes[0].id; 
+            setSelectedTodoIndex(selectedIndex); 
+        } else {
+            setSelectedTodoIndex(null); 
+        }
+    };
+
+
     return (
-        <table rules="rows">
-            <thead>
-                <tr>
-                    <th>Description</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                {todos.map((todo, index) => (
-                    <tr key={index}>
-                        <td>{todo.desc1}</td>
-                        <td>{todo.date1}</td>
-                        <td><button onClick={() => removeTodo(index)}>Remove</button></td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <div id="TodoTable">
+            
+            <div className="ag-theme-material" style={{ width: 700, height: 500 }}>
+                <AgGridReact
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
+                    rowData={todos}
+                    columnDefs={columnDefs}
+                    rowSelection="single"
+                    onSelectionChanged={onRowSelected}
+
+                />
+            </div>
+        </div>
     );
 
 
-    
+
 }
 
 export default TodoTable;

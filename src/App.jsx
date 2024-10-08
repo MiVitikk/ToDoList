6197c215bd8fd1861b4e663ba8c65d9314c6aa2e
Hilla Nokela <mikko.vitikka@myy.haaga-helia.fi>
2024-10-08 15:54:05 +0300

    Exercise enhanced

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import TodoList from './TodoList'
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Tab, Tabs} from "@mui/material";
+import {TabPanel} from "@mui/lab"
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import Box from '@mui/material/Box';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState('1');
+
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+  };
 
   return (
-    <>
-      <TodoList/>
-    </>
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static" color="transparent">
+      <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Hello" value="1" />
+            <Tab label="ToDos" value="2" />
+            
+          </TabList>
+        </Box>
+        <TabPanel value="1">Hello</TabPanel>
+        <TabPanel value="2"><TodoList />
+        <Toolbar>
+          <Typography variant="h6">
+            My Todos
+          </Typography>  
+        </Toolbar></TabPanel>
+        
+      </TabContext>
+    </Box>
+        
+
+        
+      </AppBar>
+      
+    </Container>
   )
 }
 
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,44 +3,49 @@ import TodoTable from "./TodoTable";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
-    const [newTodo, setNewTodo]  = useState({
+    const [newTodo, setNewTodo] = useState({
         desc: '',
         priority: '',
-        date: ''
+        date: null
     })
-    
+
 
     const newInput = (event) => {
-        setNewTodo({...newTodo, [event.target.name]: event.target.value});
-        
-        
-        
+        setNewTodo({ ...newTodo, [event.target.name]: event.target.value });
+
+
+
     };
-    
+    const handleDate = (newDate) => {
+        setNewTodo({ ...newTodo, date: newDate });
+    };
+
     const addTodo = () => {
-        if (!newTodo.desc || !newTodo.date){
+        if (!newTodo.desc || !newTodo.date) {
             alert('Write a description and date for a todo item')
             return;
         }
-        
-        setTodos([...todos, newTodo]);
-        setNewTodo({desc:'', priority:'', date:''})
-        
-        
 
-        
+        setTodos([...todos, newTodo]);
+        setNewTodo({ desc: '', priority: '', date: null })
+
+
+
+
     };
-    
+
 
     const [selectedTodoIndex, setSelectedTodoIndex] = useState(null);
 
-    
+
     const removeTodo = () => {
-        if (selectedTodoIndex !== null) {  
+        if (selectedTodoIndex !== null) {
             setTodos((prevTodos) => prevTodos.filter((todo, i) => i !== parseInt(selectedTodoIndex)));
             setSelectedTodoIndex(null);
         } else {
@@ -49,21 +54,37 @@ function TodoList() {
     };
 
     return (
-        
+
         <>
-        <div id="ToDoHeader">
-            <header><b>Simple Todolist</b></header>
-        </div>
-        <fieldset id="ToDoInput">
-            <legend>Add Todo</legend>
-            <input type="text" placeholder="Description" name="desc" onChange={newInput} value={newTodo.desc} />
-            <input type="text" placeholder="Priority" name="priority" onChange={newInput} value={newTodo.priority}/>
-            <input type="date" placeholder="Date" name="date" onChange={newInput} value={newTodo.date}/>
-            <button onClick={addTodo}>Add</button>
-            <button onClick={removeTodo}>Delete</button>
-        </fieldset>
-            <TodoTable todos={todos} setSelectedTodoIndex={setSelectedTodoIndex}/>
             
+
+            <fieldset id="ToDoInput">
+            <legend>Add Todo</legend>
+                <Stack
+                    spacing={2}
+                    mt={2}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    >
+                    
+                    <TextField label="Description" onChange={newInput} name="desc" value={newTodo.desc} />
+                    <TextField label="Priority" onChange={newInput} name="priority" value={newTodo.priority} />
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            label="Select date"
+                            value={newTodo.date}
+                            onChange={handleDate}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+                    <Button variant="contained" color="gray" onClick={addTodo}>Add</Button>
+                    <Button variant="contained" color="gray" onClick={removeTodo}>Delete</Button>
+                </Stack>
+            </fieldset>
+
+            <TodoTable todos={todos} setSelectedTodoIndex={setSelectedTodoIndex} />
+
         </>
     );
 }
